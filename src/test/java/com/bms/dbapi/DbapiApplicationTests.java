53569fbd030798a4c1db25dc610e0312d4864f5e
Vignesh <vvignesh10399@gmail.com>
2025-04-13 20:28:40 +0530

    first commit

---
+++ src/test/java/com/bms/dbapi/DbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.dbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
